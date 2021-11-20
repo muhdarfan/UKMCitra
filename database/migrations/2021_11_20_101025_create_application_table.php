27fc919ab762568ad29aac8bb732986fd4f7d844
WongYewKeong <a175308@siswa.ukm.edu.my>
2021-11-20 19:17:46 +0800

    create student information table and application table

---
+++ database/migrations/2021_11_20_101025_create_application_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application', function (Blueprint $table) {
+            $table->id('application_id');
+            $table->string('reason');
+            $table->date('date');
+            $table->string('status');
+            $table->string('semester');
+            $table->string('matric_no');
+            $table->string('course_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application');
+    }
+}
