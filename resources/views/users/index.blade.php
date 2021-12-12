3553375a4776f228b0627425d2025ee21ce41883
Najmi <a174863@siswa.ukm.edu.my>
2021-12-12 16:03:11 +0800

    User Information Page
    Sprint #2

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,43 +1,51 @@
 @extends('layouts.app')
 
-@section('title','Page Title')
+@section('title','User Lists')
 @section('content')
-    <!-- Default box with Title & Footer -->
+    <!-- Default data table -->
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <!-- Card Button (Remove if needed) -->
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
+            <h3 class="card-title">User Lists</h3>
         </div>
+        <!-- /.card-header -->
         <div class="card-body">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th style="width: 10%">Matric No</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th style="width: 20%">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->matric_no }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->role }}</td>
+                        <td class="text-center">
+                            <form action="{{ route('user.destroy', $user->matric_no) }}" method="POST">
+                                <a href="{{ route('user.show', $user->matric_no) }}" class="btn bg-gradient-primary">Detail</a>
+                                <a href="{{ route('user.edit', $user->matric_no) }}"
+                                   class="btn bg-gradient-warning">Edit</a>
+
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn bg-gradient-danger"
+                                        onclick="return confirm('Are you sure want to delete this user?');">Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            <br/>
+            {!! $users->withQueryString()->links() !!}
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
-    </div>
-    <!-- /.card -->
-
-    <!-- Default box -->
-    <div class="card">
-        <div class="card-body row">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
-        </div>
     </div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,43 +1,26 @@
 @extends('layouts.app')
 
-@section('title','Page Title')
+@section('title','User Information')
 @section('content')
-    <!-- Default box with Title & Footer -->
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
 
-            <!-- Card Button (Remove if needed) -->
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-        </div>
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">{{$user->matric_no}} - {{$user->name}}</h3>
+        <h3 class="card-title float-right">{{$user->role}}</h3>
+
+        <!-- Card Button (Remove if needed) -->
+        
+    </div>
+    <div class="card-body">
         <div class="card-body">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
+        <img src="{{ asset('images/img_course.png') }}" class="center-block d-block mx-auto img-fluid img-thumbnail" alt="Italian Trulli" width="600" height="500">
         </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
+        
+        <p class="text-justify">{{$user->descriptions}}</p>
     </div>
-    <!-- /.card -->
-
-    <!-- Default box -->
-    <div class="card">
-        <div class="card-body row">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged.
-        </div>
-    </div>
+    <!-- /.card-body -->
+    
+    <!-- /.card-footer-->
+</div>
+<!-- /.card -->
 @endsection
