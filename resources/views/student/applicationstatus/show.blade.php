38de700e5d92245a3ba7a95eccd3a34ead6a340b
Najmi <a174863@siswa.ukm.edu.my>
2022-01-05 23:04:16 +0800

    Application Status
    Sprint #3

--- resources/views/student/applicationstatus/show.blade.php
+++ resources/views/student/applicationstatus/show.blade.php
@@ -2,29 +2,38 @@
 
 @section('content')
 
-<div class="card">
-  <div class="card-header">
-    <h3 class="card-title">
-      <i class="fas fa-text-width"></i>
-      Application Information
-    </h3>
-  </div>
-  <!-- /.card-header -->
-  <div class="card-body">
-    <dl class="row">
-      <dt class="col-sm-4">Course Code</dt>
-      <dd class="col-sm-8">LMCR2482</dd>
-      <dt class="col-sm-4">Course Name</dt>
-      <dd class="col-sm-8">ASAS REKA BENTUK GRAFIK</dd>
-      <dt class="col-sm-4">Set</dt>
-      <dd class="col-sm-8">1</dd>
-      <dt class="col-sm-4">Reason</dt>
-      <dd class="col-sm-8">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
-        sit amet risus.
-      </dd>
-    </dl>
-  </div>
-  <!-- /.card-body -->
-</div>
+<div class="row">
+        <div class="offset-md-2 col-md-8">
+            <div class="card card-cyan">
+                <div class="card-header">
+                    <h3 class="card-title">Application Information</h3>
+                </div>
 
+                <div class="card-body">
+                    <h4>Course Information</h4>
+
+                    <div class="form-group">
+                        <label for="inputName">Course Code</label>
+                        <input type="text" readonly class="form-control-plaintext" id="inputName" placeholder="Enter name" value="{{  $application->course->courseCode }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputName">Course Name</label>
+                        <input type="text" readonly class="form-control-plaintext" id="inputName" placeholder="Enter name" value="{{ $application->course->courseName }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputReason">Reason</label>
+                        <textarea class="form-control" id="inputReason" placeholder="Reason" rows="3" style="resize: none;">{{ $application->reason }}</textarea>
+                    </div>
+                </div>
+
+                <!-- <div class="card-footer">
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
 @endsection
